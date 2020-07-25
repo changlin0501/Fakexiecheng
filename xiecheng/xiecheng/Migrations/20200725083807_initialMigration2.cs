9d662f1009ec605561bacab4cbe61700f4df7dfb
changlin0501 <guanguan19970501@gmail.com>
2020-07-25 17:03:15 +0800

    创建初始化数据库（json文件数据库）

---
+++ xiecheng/xiecheng/Migrations/20200725083807_initialMigration2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace xiecheng.Migrations
+{
+    public partial class initialMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreatTime", "DepartureTime", "Description", "DiscoutPresent", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("a623a7c3-af7a-481d-a9b5-79d08325ca81"), new DateTime(2020, 7, 25, 8, 38, 6, 958, DateTimeKind.Utc).AddTicks(7483), null, "shuoming", null, null, null, null, 0m, "Ceshititle", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("a623a7c3-af7a-481d-a9b5-79d08325ca81"));
+        }
+    }
+}
--- xiecheng/xiecheng/Migrations/20200725085100_initialMigration3.Designer.cs
+++ xiecheng/xiecheng/Migrations/20200725085100_initialMigration3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using xiecheng.Database;
 
 namespace xiecheng.Migrations
 {
     [DbContext(typeof(AppDbcontext))]
-    partial class AppDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20200725085100_initialMigration3")]
+    partial class initialMigration3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
